9fdc4ca7c03a9b415622649a2c328d8dd09c3263
Ehsan <ehsan2003.2003.382@gmail.com>
2026-02-14 04:16:34 +0330

    feat: improved nix package

--- flake.nix
+++ flake.nix
@@ -14,38 +14,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        packages.default = pkgs.stdenv.mkDerivation {
-          src = ./.;
-          preBuild = ''
-            export HOME=$(mktemp -d)
-          '';
-          buildPhase = ''
-            export HOME=$TMPDIR
-            export XDG_CACHE_HOME=$TMPDIR
-            v -prod -cflags "-O3" -o amdim amdim.v
-            strip -s amdim 
-          '';
-          postFixup = ''
-            wrapProgram $out/bin/amdim \
-              --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.umr ]} 
-          '';
-          installPhase = ''
-            mkdir -p $out/bin;
-            mv amdim $out/bin
-          '';
-          version = "1.0.0";
-          pname = "amdim";
-          buildInputs = [
-            pkgs.vlang
-            pkgs.makeWrapper
-          ];
-
-          nativeBuildInputs = [
-            pkgs.umr
-            pkgs.coreutils
-          ];
-
-        };
+        packages.default = pkgs.callPackage ./package.nix { };
       }
     );
 }
